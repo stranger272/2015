d27a67c39c84bffbdc7d0ea5b435f3defe1753d0
Joey Bricher <Joey Bricher@k-THINK>
2015-01-23 17:50:40 -0600

    Added a floor and ceil to the driving code.

--- Alice/src/org/usfirst/frc/team4215/robot/Robot.java
+++ Alice/src/org/usfirst/frc/team4215/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 
+
 /**
  * This is a demo program showing the use of the RobotDrive class.
  * The SampleRobot class is the base of a robot application that will automatically call your
@@ -24,7 +25,10 @@ import edu.wpi.first.wpilibj.Timer;
  * don't. Unless you know what you are doing, complex code will be much more difficult under
  * this system. Use IterativeRobot or Command-Based instead if you're new.
  */
-public class Robot extends SampleRobot {
+public class Robot extends SampleRobot { 
+	public static void main(String[] args) {
+		
+	}
    
 	// Objects defined for drive train.
 	Joystick leftStick = new Joystick(0);
@@ -32,12 +36,18 @@ public class Robot extends SampleRobot {
 	
 	Talon frontLeft = new Talon(0);
 	Talon backLeft = new Talon(1);
-	Talon backRight = new Talon(4);
+	Talon backRight = new Talon(2);
 	Talon frontRight = new Talon(3);
 	
-	double tank;
-	double tank2;
+	Talon elevator = new Talon(4);
+	Talon rackPinion = new Talon(5);
+	
+	double tankLeft;
+	double tankRight;
 	double strafe;
+	
+	private double MAXINPUT = .75;
+    private double MININPUT = .15;
 	
 	
     /**
@@ -58,15 +68,56 @@ public class Robot extends SampleRobot {
     }
     
     public void drivingMethod(){
+    	tankLeft = leftStick.getY();
+    	tankRight = rightStick.getY();
+    	if (rightStick.getX() >= MININPUT) {			// This line gives the right joystick strafing priority if both joysticks are moved in the x-direction.
+    		strafe = rightStick.getX();
+    	}
+    	else {
+    		strafe = .5*leftStick.getX();
+    	}
     	
-    	tank = leftStick.getY();
-    	tank2 = rightStick.getY();
-    	strafe = rightStick.getX();
     	
-    	frontLeft.set(-tank + strafe);
-    	backLeft.set(-tank - strafe);
-    	backRight.set(tank2 - strafe);
-    	frontRight.set(tank2 + strafe);
+    	// This moves the wheels on the left side of the robot.
+    	//tankLeft = Math.floor(tankLeft*20)/20;
+    	if (tankLeft <= Math.abs(MININPUT)) {
+    		tankLeft = 0;
+    	}
+    	else if (tankLeft >= MAXINPUT) {
+    		tankLeft = MAXINPUT;
+    	}
+    	else if (tankLeft <= -MAXINPUT) {
+    		tankLeft = -MAXINPUT;
+    	}
+    	
+    	// This moves the wheels on the right side of the robot.
+    	//tankRight = Math.floor(tankRight*20)/20;
+    	if (tankRight <= Math.abs(MININPUT)) {
+    		tankRight = MININPUT;
+    	}
+    	else if (tankRight >= MAXINPUT) {
+    		tankRight = MAXINPUT;
+    	}
+    	else if (tankRight <= -MAXINPUT) {
+    		tankRight = -MAXINPUT;
+    	}
+    	
+    	// This controls the strafing.
+    	if (strafe <= Math.abs(MININPUT)) {
+    		strafe = 0;
+    	}
+    	else if (strafe >= MAXINPUT) {
+    		strafe = MAXINPUT;
+    	}
+    	else if (strafe <= -MAXINPUT) {
+    		strafe = -MAXINPUT;
+    	}
+    	
+    	
+    	frontLeft.set(-tankLeft + strafe);
+    	backLeft.set(-tankLeft - strafe);
+    	backRight.set(tankRight - strafe);
+    	frontRight.set(tankRight + strafe);
     }
 
     /**
